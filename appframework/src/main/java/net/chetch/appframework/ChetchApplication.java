d3cf51b5e717b64d1dd9b243f47469d5dd1a9b05
billchetch <45006134+billchetch@users.noreply.github.com>
2023-03-19 13:23:39 +0800

    s

--- appframework/src/main/java/net/chetch/appframework/ChetchApplication.java
+++ appframework/src/main/java/net/chetch/appframework/ChetchApplication.java
@@ -1,6 +1,8 @@
 package net.chetch.appframework;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -32,5 +34,21 @@ public class ChetchApplication extends Application {
         Logger.info("Registering uce for " + getClass().getCanonicalName());
         Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler);
 
+    }
+
+    public void restartApp(int delayInSecs){
+        Intent intent = getPackageManager().getLaunchIntentForPackage( getPackageName() );
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, intent.getFlags());
+
+        AlarmManager mgr = (AlarmManager) getBaseContext().getSystemService(Context.ALARM_SERVICE);
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 1000*delayInSecs, pendingIntent);
+
+        Logger.info("Restarting app in " + delayInSecs + " seconds");
+        System.exit(0);
     }
 }
